e167f968e514926e1affe59ef61c1f4bafbb33bd
Vichu-2000 <68199051+Vichu-2000@users.noreply.github.com>
2021-05-20 21:16:21 +0530

    PATTERNS

--- patterns.c
+++ patterns.c
@@ -5,7 +5,7 @@ int main(void)
 {
    int type,rows,i,j,k;
    type=get_int("Enter type of pattern (1 or 2) ");
-   rows=get_int("Number of rows ");
+   rows=get_int("Number of rows");
     
    if(type==1){
     for(i=rows;i>0;i--){
